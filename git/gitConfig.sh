4c258d98e363164aca38e31a9c48481185c6ffc1
pGrimaud <pascalgrimaud@gmail.com>
2015-04-19 11:02:27 +0200

    improve the information display

--- git/gitConfig.sh
+++ git/gitConfig.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+function echoInfoGit {
+	username=$(git config --global user.name $1)
+	useremail=$(git config --global user.email $2)
+	echo user.name=$username
+	echo user.email=$useremail
+}
+
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <username> <email>" >&2
-  cat ~/.gitconfig
-  exit 1
+	echo "Usage: $0 <username> <email>" >&2
+	echoInfoGit
+	exit 1
 fi
 
 git config --global user.name $1
 git config --global user.email $2
-cat ~/.gitconfig
+echoInfoGit
 
