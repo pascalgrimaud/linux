289fdea47e73054f62ac0dfadd9a297daac0f470
pGrimaud <pascalgrimaud@gmail.com>
2015-05-03 18:02:08 +0200

    add new themes

---
+++ themes/emeraldIconsInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# from : http://gnome-look.org/
+sudo add-apt-repository ppa:noobslab/icons -y
+sudo apt-get -y update
+sudo apt-get -y install emerald-icon-theme
+
---
+++ themes/fsIconsInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# from : http://gnome-look.org/
+sudo add-apt-repository ppa:noobslab/icons2
+sudo apt-get update
+sudo apt-get install fs-icons
