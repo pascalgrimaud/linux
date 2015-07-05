ee8fdf3e7681722524859bcd42b3cc67d764f084
pGrimaud <pascalgrimaud@gmail.com>
2015-07-05 19:48:03 +0200

    add java install 5, 6, 7

---
+++ java/java5Install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# update
+sudo apt-get -y update
+
+# install Java 5
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+  -O /tmp/jdk-1_5_0_22-linux-amd64.bin http://download.oracle.com/otn-pub/java/jdk/1.5.0_22/jdk-1_5_0_22-linux-amd64.bin
+echo yes|sh /tmp/jdk-1_5_0_22-linux-amd64.bin
+sudo mv jdk1.5.0_22 /usr/lib/jvm/
+sudo chown root:root /usr/lib/jvm/jdk1.5.0_22/
