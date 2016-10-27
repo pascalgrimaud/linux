69e109fc68a9ad422a295f3912a70e93049f45f0
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-10-27 07:05:19 +0200

    Update minikube (0.10.0 to 0.12.0) and kubectl (1.3.0 to 1.4.4)

--- kubernetes/kubectl_install.sh
+++ kubernetes/kubectl_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.3.0
+VERSION=1.4.4
 
 # http://kubernetes.io/docs/getting-started-guides/minikube/#download-kubectl
 curl -sSL "http://storage.googleapis.com/kubernetes-release/release/v${VERSION}/bin/linux/amd64/kubectl" > kubectl
--- kubernetes/minikube_install.sh
+++ kubernetes/minikube_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.10.0
+VERSION=0.12.0
 
 # https://github.com/kubernetes/minikube/releases
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
