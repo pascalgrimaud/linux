ee682f9a105ebe39aa3f17d1c04738a8a11dff92
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-11-12 22:46:33 +0100

    Update kubernetes installation

--- kubernetes/kubectl_install.sh
+++ kubernetes/kubectl_install.sh
@@ -3,12 +3,6 @@
 # put the version -> beginning with 'v' letter
 VERSION=v1.4.6
 
-# make sure the package repository is up to date
-sudo apt-get -y update
-
-# install wget
-sudo apt-get -y install curl
-
 # http://kubernetes.io/docs/getting-started-guides/minikube/#download-kubectl
 curl -sSL "http://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubectl" > kubectl
 sudo mv kubectl /usr/bin/kubectl
--- kubernetes/minikube_install.sh
+++ kubernetes/minikube_install.sh
@@ -3,11 +3,7 @@
 # put the version -> beginning with 'v' letter
 VERSION=v0.12.2
 
-# make sure the package repository is up to date
-sudo apt-get -y update
-
-# install wget
-sudo apt-get -y install curl
-
 # https://github.com/kubernetes/minikube/tags
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/${VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/${VERSION}/minikube-linux-amd64 && \
+    chmod +x minikube && \
+    sudo mv minikube /usr/local/bin/
