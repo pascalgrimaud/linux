4b37971bd582e563ee678c662ae7dc9ec087cfb5
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-11-12 22:46:01 +0100

    Update IDE

--- ide/eclipseLuna_download.sh
+++ ide/eclipseLuna_download.sh
@@ -7,4 +7,3 @@ fi
 
 cd $1
 wget http://mirror.ibcp.fr/pub/eclipse/technology/epp/downloads/release/luna/SR2/eclipse-jee-luna-SR2-linux-gtk-x86_64.tar.gz
-
--- ide/ideaIC14_download.sh
+++ ide/ideaIC14_download.sh
@@ -7,4 +7,3 @@ fi
 
 cd $1
 wget https://download.jetbrains.com/idea/ideaIU-14.1.7.tar.gz
-
