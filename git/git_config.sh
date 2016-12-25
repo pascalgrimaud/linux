a2a44b2c2d137b67a2df755a295c79925b6ac647
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-12-25 17:49:24 +0100

    Update git

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 function echoInfoGit {
-	username=$(git config --global user.name)
-	useremail=$(git config --global user.email)
-	echo " "
-	echo user.name="$username"
-	echo user.email="$useremail"
-	echo " "
+    username=$(git config --global user.name)
+    useremail=$(git config --global user.email)
+    echo " "
+    echo user.name="$username"
+    echo user.email="$useremail"
+    echo " "
 }
 
 if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 \"<username>\" <email>" >&2
-	echoInfoGit
-	exit 1
+    echo "Usage: $0 \"<username>\" <email>" >&2
+    echoInfoGit
+    exit 1
 fi
 
 git config --global user.name "$1"
