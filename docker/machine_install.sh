c868cf5abd33ff64074083849376a49e7e9424ae
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-12-25 17:34:39 +0100

    Update docker

--- docker/machine_install.sh
+++ docker/machine_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # put the version -> beginning with 'v' letter
-VERSION=v0.9.0-rc1
+VERSION=v0.9.0-rc2
 
 # Install docker-machine
 curl -L https://github.com/docker/machine/releases/download/"$VERSION"/docker-machine-"$(uname -s)"-"$(uname -m)" > docker-machine
