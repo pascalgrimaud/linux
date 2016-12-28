4ab9860d825dbfd01324dc3e3fd308152c6c66a2
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-12-28 07:03:56 +0100

    Update kubectl and minikube

--- kubernetes/minikube_install.sh
+++ kubernetes/minikube_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # put the version -> beginning with 'v' letter
-VERSION=v0.12.2
+VERSION=v0.14.0
 
 # https://github.com/kubernetes/minikube/tags
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/${VERSION}/minikube-linux-amd64 && \
