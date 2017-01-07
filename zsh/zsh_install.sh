3e3f7d03713e113db29bd4db5990bb57c336cd46
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-01-07 23:38:34 +0100

    Add script to install zsh

---
+++ zsh/zsh_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install zsh
+sudo apt-get install -y zsh
+
+# install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+echo 'SHELL=/bin/zsh' >> /etc/environment
