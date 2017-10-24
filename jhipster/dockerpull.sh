7c6e615753e00133642d7dc77307d64f8b63f34b
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-10-24 23:11:37 +0200

    Update dockerpull

--- jhipster/dockerpull.sh
+++ jhipster/dockerpull.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-docker pull jhipster/jhipster-registry:v2.5.7
+docker pull jhipster/jhipster-registry:v3.1.2
 docker pull openjdk:8-jre-alpine
-docker pull mysql:5.7.13
+docker pull mysql:5.7.19
 docker pull mariadb:10.1.17
-docker pull postgres:9.5.4
-docker pull mongo:3.2.10
+docker pull postgres:9.6.5
+docker pull mongo:3.4.8
 docker pull cassandra:3.9
 docker pull elasticsearch:2.4.1
-docker pull wurstmeister/kafka:0.10.0.1
+docker pull wurstmeister/kafka:0.10.1.1
 docker pull wurstmeister/zookeeper:3.4.6
-docker pull sonarqube:6.1-alpine
-docker pull jhipster/jhipster-console:v1.3.2
-docker pull jhipster/jhipster-elasticsearch:v1.3.2
-docker pull jhipster/jhipster-logstash:v1.3.2
-docker pull consul:v0.7.2
-docker pull jhipster/consul-config-loader:v0.2.0
+docker pull sonarqube:6.5-alpine
+docker pull jhipster/jhipster-console:v2.2.1
+docker pull jhipster/jhipster-elasticsearch:v2.2.1
+docker pull jhipster/jhipster-logstash:v2.2.1
+docker pull consul:0.9.3
+docker pull jhipster/consul-config-loader:v0.2.2
 
 docker pull openjdk:8-jdk-alpine
 
-docker pull jhipster/jhipster:v3.12.2
+docker pull jhipster/jhipster:v4.10.0
